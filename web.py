29b7cdb4435201caaf7415e0493d323c7c5f7c23
Brian Joseph <brianjoseph1758@gmail.com>
2024-06-18 16:05:26 -0400

    Remove period

--- web.py
+++ web.py
@@ -26,5 +26,5 @@ for index, todo in enumerate(todos):
        del st.session_state[todo]
        st.experimental_rerun()
 
-st.text_input(label="Enter a todo:", placeholder="Add new todo....",
+st.text_input(label="Enter a todo:", placeholder="Add new todo...",
               on_change=add_todo, key= "new_todo")
